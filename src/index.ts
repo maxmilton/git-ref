27825bdf2e4d715fa2ab1a8d069f564537caa291
Max Milton <max@wearegenki.com>
2021-06-07 08:42:20 +1000

    Add new gitHash function to get the git hash

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 
 /**
- * Get the current git reference via `git describe`.
+ * Get the HEAD git reference via `git describe`.
  *
  * @see <https://git-scm.com/docs/git-describe>
  *
@@ -24,3 +24,24 @@ export function gitRef(args = '--dirty="-dev" --broken'): string {
 }
 
 export default gitRef;
+
+/**
+ * Get the HEAD commit hash.
+ *
+ * @param long - Get the full git hash instead of a short hash in the form of
+ * the first 7 characters (default `false`).
+ * @returns The git commit hash.
+ */
+export function gitHash(long?: boolean): string {
+  let hash = '';
+
+  try {
+    const result = execSync(`git rev-parse${long ? '' : ' --short'} HEAD`) || '';
+    hash = result.toString().trim();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+
+  return hash;
+}
