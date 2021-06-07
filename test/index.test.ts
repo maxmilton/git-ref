9e444759c4faf8bc1c60bc6c1f769f059cd65578
Max Milton <max@wearegenki.com>
2021-06-07 10:49:42 +1000

    Add tests to check results in various git tree conditions

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,13 @@
 import * as assert from 'uvu/assert';
 import * as allExports from '../src/index';
 import { gitHash, gitRef } from '../src/index';
-import { describe } from './utils';
+import {
+  createTempDir,
+  deleteTempDir,
+  describe,
+  execCmds,
+  getTempDir,
+} from './utils';
 
 describe('exports', (test) => {
   (
@@ -22,32 +28,102 @@ describe('exports', (test) => {
   });
 });
 
-// TODO: Run in various other sample git repos, e.g.:
-// - without any commits
-// - with commits but without any tags
-// - with multiple tags
-// - with a dirty tree (uncommitted changes)
-// - in a broken repo
-// - not in a git repo
-// - in a git repo with commits but no HEAD ref
-
 describe('gitRef', (test) => {
+  test.before(createTempDir);
+  test.after(deleteTempDir);
+
   test('returns a non-empty string', () => {
     const result = gitRef();
-    assert.is.not(result, '');
     assert.type(result, 'string');
+    assert.is.not(result, '');
   });
 
   test('returns a tag git reference', () => {
     const result = gitRef();
     assert.is(/^v\d+\.\d+\.\d+/.test(result), true, 'matches expected format');
   });
+
+  test('returns empty string in non-git dir', () => {
+    const dir = getTempDir('not-git');
+    const result = gitRef(undefined, dir);
+    assert.is(result, '');
+  });
+
+  test('returns empty string in repo without commits', () => {
+    const dir = getTempDir('no-commit');
+    execCmds(dir, ['git init --quiet']);
+    const result = gitRef(undefined, dir);
+    assert.is(result, '');
+  });
+
+  test('returns short hash string in repo with commit but no tag', () => {
+    const dir = getTempDir('with-commit');
+    execCmds(dir, [
+      'git init --quiet',
+      'touch file.txt',
+      'git add file.txt',
+      'git commit --quiet --no-gpg-sign -m "commit1"',
+    ]);
+    const result = gitRef(undefined, dir);
+    assert.type(result, 'string');
+    assert.is(result.length, 7);
+  });
+
+  test('returns tag string in repo with tag', () => {
+    const dir = getTempDir('with-tag');
+    execCmds(dir, [
+      'git init --quiet',
+      'touch file.txt',
+      'git add file.txt',
+      'git commit --quiet --no-gpg-sign -m "commit1"',
+      'git tag --no-sign -m "v123" v123',
+    ]);
+    const result = gitRef(undefined, dir);
+    assert.is(result, 'v123');
+  });
+
+  test('appends "-dev" in repo with dirty tree (uncommitted changes)', () => {
+    const dir = getTempDir('dirty-tree');
+    execCmds(dir, [
+      'git init --quiet',
+      'touch file.txt',
+      'git add file.txt',
+      'git commit --quiet --no-gpg-sign -m "commit1"',
+      'touch file2.txt',
+      'git add file2.txt',
+    ]);
+    const result = gitRef(undefined, dir);
+    assert.type(result, 'string');
+    assert.ok(result.endsWith('-dev'));
+  });
+
+  test('appends "-broken" in repo with broken tree', () => {
+    const dir = getTempDir('broken-tree');
+    execCmds(dir, [
+      'git init --quiet',
+      'touch file.txt',
+      'git add file.txt',
+      'git commit --quiet --no-gpg-sign -m "commit1"',
+      "sed -i 's/file.txt/corrupted.txt/g' ./.git/index",
+    ]);
+    const result = gitRef(undefined, dir);
+    assert.type(result, 'string');
+    assert.ok(result.endsWith('-broken'));
+  });
 });
 
 describe('gitHash', (test) => {
-  test('returns a short git hash by default', () => {
+  test.before(createTempDir);
+  test.after(deleteTempDir);
+
+  test('returns a non-empty string', () => {
     const result = gitHash();
     assert.type(result, 'string');
+    assert.is.not(result, '');
+  });
+
+  test('returns a short git hash by default', () => {
+    const result = gitHash();
     assert.is(result.length, 7);
   });
 
@@ -61,5 +137,18 @@ describe('gitHash', (test) => {
     const result = gitHash(false);
     assert.type(result, 'string');
     assert.is(result.length, 7);
+  });
+
+  test('returns empty string in non-git dir', () => {
+    const dir = getTempDir('not-git');
+    const result = gitHash(undefined, dir);
+    assert.is(result, '');
+  });
+
+  test('returns empty string in repo without commits', () => {
+    const dir = getTempDir('no-commit');
+    execCmds(dir, ['git init --quiet']);
+    const result = gitHash(undefined, dir);
+    assert.is(result, '');
   });
 });
--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-
+import { execSync } from 'child_process';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
 import { suite } from 'uvu';
 
 export function describe(
@@ -10,3 +12,51 @@ export function describe(
   fn(testSuite);
   testSuite.run();
 }
+
+let tmpDir: string | undefined;
+
+export async function createTempDir(): Promise<void> {
+  tmpDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'git-ref-test-'));
+}
+
+export async function deleteTempDir(): Promise<void> {
+  if (!tmpDir) {
+    throw new Error(
+      'No temp directory exists, you need to call createTempDir() first',
+    );
+  }
+
+  await fs.promises.rm(tmpDir, {
+    force: true,
+    recursive: true,
+  });
+
+  tmpDir = undefined;
+}
+
+export function getTempDir(subDir?: string): string {
+  if (!tmpDir) {
+    throw new Error(
+      'No temp directory exists, you need to call createTempDir() first',
+    );
+  }
+
+  if (subDir) {
+    const newDir = path.join(tmpDir, subDir);
+    fs.mkdirSync(newDir);
+    return newDir;
+  }
+
+  return tmpDir;
+}
+
+export function execCmds(dir: string, cmds: string[]): void {
+  cmds.forEach((cmd) => {
+    execSync(cmd, {
+      cwd: dir,
+      timeout: 2000,
+    });
+  });
+}
+
+// export function runInDir(dir:string, cb)
