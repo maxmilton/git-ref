75b3403e05d8e9cf4a4bf09c374354dae4793ff9
Max Milton <max@wearegenki.com>
2021-06-07 10:48:56 +1000

    Support passing custom cwd path

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,15 @@ import { execSync } from 'child_process';
  *
  * @param args - Additional arguments to pass to `git describe`. The default
  * is `'--dirty="-dev" --broken'`.
+ * @param cwd - Modify the working directory git is executed in (default is the
+ * directory of the current node process).
  * @returns A human readable git reference.
  */
-export function gitRef(args = '--dirty="-dev" --broken'): string {
+export function gitRef(args = '--dirty="-dev" --broken', cwd?: string): string {
   let reference = '';
 
   try {
-    const result = execSync(`git describe --always ${args}`) || '';
+    const result = execSync(`git describe --always ${args}`, { cwd });
     reference = result.toString().trim();
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -30,13 +32,17 @@ export default gitRef;
  *
  * @param long - Get the full git hash instead of a short hash in the form of
  * the first 7 characters (default `false`).
+ * @param cwd - Modify the working directory git is executed in (default is the
+ * directory of the current node process).
  * @returns The git commit hash.
  */
-export function gitHash(long?: boolean): string {
+export function gitHash(long?: boolean, cwd?: string): string {
   let hash = '';
 
   try {
-    const result = execSync(`git rev-parse${long ? '' : ' --short'} HEAD`) || '';
+    const result = execSync(`git rev-parse${long ? '' : ' --short'} HEAD`, {
+      cwd,
+    });
     hash = result.toString().trim();
   } catch (error) {
     // eslint-disable-next-line no-console
