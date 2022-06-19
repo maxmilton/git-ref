9cac1769a02c53e5b3a33f9c7a39d38f131e5cb7
Max Milton <max@happycode.ltd>
2022-06-20 06:56:58 +1000

    chore: Remove unnecessary eslint config option

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,12 @@
 const ERROR = 2;
 
-// TODO: Types
-// eslint-disable-next-line max-len
-// /** @type {import('eslint/lib/shared/types').ConfigData & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
 module.exports = {
   root: true,
   reportUnusedDisableDirectives: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    extraFileExtensions: ['.mjs', '.cjs'],
     project: ['./test/tsconfig.json'],
+    tsconfigRootDir: __dirname,
   },
   extends: [
     'eslint:recommended',
