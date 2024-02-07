9dbdcf8f864c17ec2bfb01801cbfdac00bf2fb57
Max Milton <max@happyhackers.xyz>
2024-02-08 07:29:28 +1100

    chore: Fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+'use strict'; // eslint-disable-line
+
 const OFF = 0;
 const WARN = 1;
 const ERROR = 2;
@@ -19,17 +21,23 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:unicorn/recommended',
-    'prettier',
   ],
-  plugins: ['prettier'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': ERROR,
     'import/prefer-default-export': OFF,
     'no-restricted-syntax': OFF,
-    'prettier/prettier': WARN,
     'unicorn/filename-case': OFF,
     'unicorn/no-abusive-eslint-disable': WARN,
     'unicorn/no-null': OFF,
     'unicorn/prevent-abbreviations': OFF,
+
+    /* Covered by biome formatter */
+    '@typescript-eslint/indent': OFF,
+    'function-paren-newline': OFF,
+    'implicit-arrow-linebreak': OFF,
+    'max-len': OFF,
+    'object-curly-newline': OFF,
+    'operator-linebreak': OFF,
+    'unicorn/no-nested-ternary': OFF,
   },
 };
